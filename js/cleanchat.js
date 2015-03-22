bd4ff7c395bb25ef2229dd1ebc7163dee8eeec3d
AuriaIT <github@pizzaalarm.de>
2015-03-22 10:16:28 +0100

    bugfix js

--- js/cleanchat.js
+++ js/cleanchat.js
@@ -7,7 +7,7 @@
 var _fingerprint = new Fingerprint().get();
 var _dweet_name = "auria_it.chat";
 
-<<<<<<< HEAD
+
         var _fingerprint = new Fingerprint().get();
         var _dweet_name = "auria_it.chat";
   
@@ -22,21 +22,15 @@ var _dweet_name = "auria_it.chat";
 		
         $(".my_fingerprint").html(_fingerprint);
         
-        dweetio.get_latest_dweet_for(_dweet_name, function(err, dweet){
-=======
-/**
-* Enables the background mode. The app will not pause while in background.
-*/
-if ( window.plugin ) {
-	window.plugin.backgroundMode.enable();
-}
->>>>>>> origin/master
+       
+		dweetio.get_latest_dweet_for(_dweet_name, function(err, dweet){
 
-$(".my_fingerprint").html(_fingerprint);
+			var dweet = dweet[0]; // Dweet is always an array of 1
+			_prependMessage(dweet);
 
-<<<<<<< HEAD
-        });
-      
+		});
+
+
         dweetio.listen_for(_dweet_name, function(dweet){
             
             _prependMessage(dweet);
@@ -87,64 +81,5 @@ $(".my_fingerprint").html(_fingerprint);
           }
          return str;
          }
-		 
-=======
-dweetio.get_latest_dweet_for(_dweet_name, function(err, dweet){
-
-	var dweet = dweet[0]; // Dweet is always an array of 1
-	_prependMessage(dweet);
-
-});
-
-dweetio.listen_for(_dweet_name, function(dweet){
 	
-	_prependMessage(dweet);
-	
-});
-
-var _postChat = function (new_message) {
-	document.getElementById('my_message').value = "";
-	
-	if ( new_message ) {
-		dweetio.dweet_for(_dweet_name, {"the_message":new_message,"sender_id":_fingerprint}, function(err, dweet){});  
-	}   
-	
-	return false;
-}
-
-var _prependMessage = function (dweet) {
-
-	var classstyle = "alert-info";
-	
-	if ( dweet ) {
-			   
-		if (dweet["content"] != "" ) {                
-			
-			if ( dweet["content"]["sender_id"] == _fingerprint ) {
-				classstyle = "alert-success";
-			} else {
-				if ( window.plugin) {    
-					window.plugin.notification.local.add({ message: "Neue Nachricht" })
-				}		
-			}
-		
-			$(".message_container").prepend('<div style="font-size:10px;">'+ htmlspecialchars('@ ' + dweet["created"].toLocaleDateString("de-DE") + ' ' +dweet["created"].toLocaleTimeString("de-DE") + ' from ' + dweet["content"]["sender_id"]) +'</div><div class="alert '+classstyle+'" style="margin-top:2px;padding:5px;">' + htmlspecialchars(dweet["content"]["the_message"]) + '</div>');
-
-		}        
-	}
-}
-
-
-function htmlspecialchars(str) {
- if (typeof(str) == "string") {
-  str = str.replace(/&/g, "&amp;"); /* must do &amp; first */
-  str = str.replace(/"/g, "&quot;");
-  str = str.replace(/'/g, "&#039;");
-  str = str.replace(/</g, "&lt;");
-  str = str.replace(/>/g, "&gt;");
-  }
- return str;
- }
- 
->>>>>>> origin/master
 	 
